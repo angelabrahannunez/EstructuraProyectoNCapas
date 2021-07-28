a4c9272c052e0d55536f39866828ff5897f5cc53
Angel Abraham Nuñez Feliz <angelabrahannunez@gmail.com>
2021-07-27 23:25:07 -0400

    Estructura proyecto N capas, ci de bitbukect, .editorconfig, .gitignore and readme.md

---
+++ RHSystem/Entity/Users/Class1.cs
@@ -0,0 +1,6 @@
+namespace Entity
+{
+    public class Class1
+    {
+    }
+}
---
+++ RHSystem/Views/Users/Form1.cs
@@ -0,0 +1,9 @@
+using System.Windows.Forms;
+
+namespace Views
+{
+    public partial class Form1 : Form
+    {
+        public Form1() => InitializeComponent();
+    }
+}
